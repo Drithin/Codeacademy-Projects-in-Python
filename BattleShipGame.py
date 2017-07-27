521abcb0fbc089c1252e4acf272dbf191da32e32
Drithin <drithn@gmail.com>
2017-07-27 08:30:18 -0700

    feat: Coded a Game Over for end of all turns

--- BattleShipGame.py
+++ BattleShipGame.py
@@ -44,4 +44,6 @@ for turn in range(4):
 	           print "You missed my battleship!"
 	           board[guess_row][guess_col] = "X"
 	       print "Turn", turn+1# Print (turn + 1) here!
+	       if turn == 3:
+	       	  print "Game Over"
 	       print_board(board)
