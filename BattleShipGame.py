c96816c15563af8087bfbefc9f522dee3e5e5b2a
Drithin <drithn@gmail.com>
2017-07-27 08:06:53 -0700

    chore: COded an elif to notify player of already guessed position

--- BattleShipGame.py
+++ BattleShipGame.py
@@ -31,7 +31,13 @@ print ship_col
 if guess_row == ship_row and guess_col == ship_col:
     print "Congratulations! You sank my battleship!"
 else:
+    if guess_row not in range(5) or guess_col not in range(5):
+        print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col]=="X":
+        print "You guessed that one already."
+    else:
+        print "You missed my battleship"
     print "You missed my battleship!"
     board[guess_row][guess_col] = "X"
     print_board(board[guess_row][guess_col])
-    
+
