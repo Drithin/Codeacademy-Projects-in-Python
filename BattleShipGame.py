d2ac1ecc4f22c77b24de3beb48f837b8a3eb9031
Drithin <drithn@gmail.com>
2017-07-27 08:32:14 -0700

    chore: Added break statement after win statement to break out of the loop

--- BattleShipGame.py
+++ BattleShipGame.py
@@ -35,6 +35,7 @@ for turn in range(4):
 	   guess_col = int(raw_input("Guess Col:"))
 	   if guess_row == ship_row and guess_col == ship_col:
 	       print "Congratulations! You sunk my battleship!"
+	       break
 	   else:
 	       if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
 	           print "Oops, that's not even in the ocean."
