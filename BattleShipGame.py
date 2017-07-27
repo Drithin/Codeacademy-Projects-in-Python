f7580e2d73a4f90c1d9be010314caca85599848e
Drithin <drithn@gmail.com>
2017-07-27 08:22:19 -0700

    feat: Coded a max of 4 turns for player

--- BattleShipGame.py
+++ BattleShipGame.py
@@ -28,16 +28,20 @@ print ship_row
 print ship_col
 
 
-if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
-else:
-    if guess_row not in range(5) or guess_col not in range(5):
-        print "Oops, that's not even in the ocean."
-    elif board[guess_row][guess_col]=="X":
-        print "You guessed that one already."
-    else:
-        print "You missed my battleship"
-    print "You missed my battleship!"
-    board[guess_row][guess_col] = "X"
-    print_board(board[guess_row][guess_col])
-
+for turn in range(4):
+	   
+	   
+	   guess_row = int(raw_input("Guess Row:"))
+	   guess_col = int(raw_input("Guess Col:"))
+	   if guess_row == ship_row and guess_col == ship_col:
+	       print "Congratulations! You sunk my battleship!"
+	   else:
+	       if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+	           print "Oops, that's not even in the ocean."
+	       elif(board[guess_row][guess_col] == "X"):
+	           print "You guessed that one already."
+	       else:
+	           print "You missed my battleship!"
+	           board[guess_row][guess_col] = "X"
+	       print "Turn", turn+1# Print (turn + 1) here!
+	       print_board(board)
